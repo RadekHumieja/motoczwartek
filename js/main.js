7f4a68084c51c8055d847948a43f9191fd832fa1
Radek Humieja <rad.hum@iclude.com>
2024-04-11 21:09:55 +0200

    time

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const daysCard = document.querySelector('.time_card-days');
+const hoursCard = document.querySelector('.time_card-hours');
+const minutesCard = document.querySelector('.time_card-minutes');
+
+let usersTime = new Date(2024, 3, 20, 9);
+
+const setTime = () => {
+	const currentTime = new Date();
+	const result = usersTime - currentTime;
+
+	const days = Math.floor(result / 1000 / 60 / 60 / 24);
+	const hours = Math.floor(result / 1000 / 60 / 60) % 24;
+	const minutes = Math.floor(result / 1000 / 60) % 60;
+
+	console.log(`Dni: ${days}`);
+	console.log(`Godziny: ${hours}`);
+	console.log(`Minuty: ${minutes}`);
+
+	daysCard.textContent = days;
+	hoursCard.textContent = hours;
+	minutesCard.textContent = minutes;
+};
+setTime();
+
+setInterval(() => {
+	setTime();
+}, 60000);
